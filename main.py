56085c595e23226baffe96c6ed4b66681aba864a
Veni <63829007+midnightcuberx@users.noreply.github.com>
2020-05-25 21:35:18 +1200

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,25 @@
-import discord
-import random
+import discord,random,keep_alive,gspread,corona_api,json,os,time,datetime,pymongo,dns
 from discord.ext import commands, tasks
-import gspread
-import corona_api
-from oauth2client.service_account import ServiceAccountCredentials
-import json
-import os
 from itertools import cycle
-import time
-import datetime
 
 
 start_time=time.time()
 
-
+mongosecret=os.environ.get("mongosecret")
+client = pymongo.MongoClient(mongosecret)
+db = client["bot"]
 
 def get_prefix(bot,message):
-  with open('prefixes.json','r') as f:
-    prefixes=json.load(f)
+  collection=db["prefix"]
   try:
-    hi=prefixes[str(message.guild.id)]
-  except KeyError:
+    collection.insert_one({"_id":message.guild.id,"prefix":"+"})
     return "+"
-  return hi
+  except pymongo.errors.DuplicateKeyError:
+    collection=db["prefix"]
+    results=collection.find({"_id":message.guild.id})
+    for result in results:
+      prefix=result
+    return prefix["prefix"]
 
 bot = commands.Bot(command_prefix= get_prefix)
 status=cycle(["with Rubiks cubes","+help","ping me for help","with 11DTP","with corona","with myself","with my gf"])
@@ -44,11 +41,17 @@ async def on_ready():
 
 @bot.event
 async def on_guild_join(guild):
-  with open('prefixes.json','r') as f:
-    prefixes=json.load(f)
-  prefixes[str(guild.id)]="+"  
-  with open('prefixes.json','w') as f:
-    json.dump(prefixes,f,indent=4)
+  collection=db["prefix"]
+  collection.insert_one({"_id":guild.id,"prefix":"+"})
+  db=client["comp"]
+  collection=db["results"]
+  collection.insert_one({"_id":guild.id,"3x3":{},"4x4":{},"2x2":{},"5x5":{},"6x6":{},"7x7":{},"square-1":{},"skewb":{},"clock":{},"pyraminx":{},"oh":{},"megaminx":{},"3bld":{},"4bld":{},"5bld":{}})
+  db=client["comp"]
+  collection=db["config"]
+  collection.insert_one({"_id":guild.id,"3x3":{"mode":"on"},"4x4":{"mode":"on"},"2x2":{"mode":"on"},"5x5":{"mode":"on"},"6x6":{"mode":"on"},"7x7":{"mode":"on"},"square-1":{"mode":"on"},"skewb":{"mode":"on"},"clock":{"mode":"on"},"pyraminx":{"mode":"on"},"oh":{"mode":"on"},"megaminx":{"mode":"on"},"3bld":{"mode":"on"},"4bld":{"mode":"on"},"5bld":{"mode":"on"}})
+  db=client["comp"]
+  collection=db.bans
+  collection.insert_one({"_id":guild.id,"bans":{}})
   with open('dadtoggle.json','r') as a:
     dad=json.load(a)
   dad[str(guild.id)]="off"
@@ -62,11 +65,17 @@ async def on_guild_join(guild):
 
 @bot.event
 async def on_bot_remove(guild):
-  with open('prefixes.json','r') as f:
-    prefixes=json.load(f)
-  prefixes.pop(str(guild.id))  
-  with open('prefixes.json','w') as f:
-    json.dump(prefixes,f,indent=4)
+  collection=db["prefix"]
+  collection.delete_one({"_id":guild.id})
+  db=client["comp"]
+  collection=db["results"]
+  collection.delete_one({"_id":guild.id})
+  db=client["comp"]
+  collection=db["config"]
+  collection.delete_one({"_id":guild.id})
+  db=client["comp"]
+  collection=db.bans
+  collection.delete_one({"_id":guild.id})
   with open('dadtoggle.json','r') as a:
     dad=json.load(a)
   dad.pop(str(guild.id))
@@ -80,13 +89,19 @@ async def on_bot_remove(guild):
   
 @bot.event
 async def on_message(message):
+  collection=db["prefix"]
   if message.author.bot:
     return
   elif "<@!679529602028273740>" in message.content or "<@679529602028273740>" in message.content:
-    with open('prefixes.json','r') as f:
-      prefix=json.load(f)
-      serverprefix=prefix[str(message.guild.id)]
-    await message.channel.send(f"My prefix in {message.guild} is {serverprefix} Do {serverprefix}help for more info on my commands")
+    try:
+      collection.insert_one({"_id":message.guild.id,"prefix":"+"})
+      prefix= "+"
+    except pymongo.errors.DuplicateKeyError:
+      collection=db["prefix"]
+      results=collection.find({"_id":message.guild.id})
+      for result in results:
+        prefix=result["prefix"]
+    await message.channel.send(f"My prefix in {message.guild} is {prefix} Do {prefix}help for more info on my commands")
   elif message.content.startswith(get_prefix(bot,message)):
     with open('economy.json','r') as b:
       economy=json.load(b)
@@ -112,46 +127,19 @@ async def on_message(message):
     economy[str(message.author.id)]["maxbank"]+=random.randint(40,60)
     with open('economy.json','w') as b:
       json.dump(economy,b,indent=4)
-  if message.guild.id!=696453389625720873:
-    await bot.process_commands(message)
-    return
-  with open('number.json','r') as f:
-    num=json.load(f)
-  try:
-    trynum=int(message.content)
-  except ValueError:
-    await bot.process_commands(message)
-    return
-
-  if trynum==num["number"]:
-    await message.channel.send(f"{message.author.mention}, You guessed the number I was thinking of! It was {trynum}!")
-    num["number"]=random.randint(1,100)
-  else:
-    if trynum==num["previous"]:
-      await message.channel.send(f"{trynum} is nor closer or further than {trynum} to the number I am thinking of")
-    else:
-      diff1=num["number"]-trynum
-      diff2=num["number"]-num["previous"]
-      if diff1<0:
-        diff1=diff1/(-1)
-      if diff2<0:
-        diff2=diff2/(-1)
-      if diff1<diff2:
-        await message.channel.send(f"{trynum} is closer to my number than {num['previous']}")
-      elif diff1==diff2:
-        await message.channel.send(f"{trynum} and {num['previous']} are equally as close as each other to my number")
-      else:
-        await message.channel.send(f"{num['previous']} is closer to my number than {trynum}")
-
-  num["previous"]=trynum
-  with open('number.json','w') as f:
-    json.dump(num,f,indent=4)
 
   await bot.process_commands(message)
 
 bot.remove_command('help')
 
 
+@bot.command()
+@commands.is_owner()
+async def prefixes(ctx):
+  for guild in bot.guilds:
+    collection=db["prefix"]
+    collection.insert_one({"_id":guild.id,"prefix":"+"})
+  await ctx.send("DOne")
 
 
 @bot.command()
@@ -196,12 +184,6 @@ for filename in os.listdir('./cogs'):
     bot.load_extension(f"cogs.{filename[:-3]}")
 
 @bot.command()
-async def test(ctx):
-  if ctx.channel.is_nsfw():
-    await ctx.send("This channel is nsfw")
-  else:
-    await ctx.send("This channel is not NSFw")
-@bot.command()
 async def guild(ctx):
   for guild in bot.guilds:
     await ctx.send(guild.id)
@@ -210,17 +192,17 @@ async def guild(ctx):
 @bot.command(aliases=["prefix","newprefix"])
 @commands.has_permissions(administrator=True)
 async def setprefix(ctx,newprefix):
-  with open('prefixes.json','r') as f:
-    prefixes=json.load(f)
-  prefixes[str(ctx.guild.id)]=newprefix
-  with open('prefixes.json','w') as f:
-    json.dump(prefixes,f,indent=4)
+  collection=db["prefix"]
+  try:
+    collection.insert_one({"_id":ctx.guild.id,"prefix":newprefix})
+  except pymongo.errors.DuplicateKeyError:
+    collection.update_one({"_id":ctx.guild.id},{"$set":{"prefix":newprefix}})
   await ctx.send(f'prefix is changed to {newprefix}')
 @setprefix.error
 async def setprefix_error(ctx,error):
   if isinstance(error,commands.MissingPermissions):
     await ctx.send("You do not have permission to change my prefix! You need admin to do so.")
-  elif isinstance(error,commands.MissingArgument):
+  elif isinstance(error,commands.MissingRequiredArgument):
     await ctx.send("You did not specify what to change the prefix to!")
 
 
@@ -234,6 +216,8 @@ async def shutdown(ctx):
 async def shutdown_error(ctx,error):
   if isinstance(error,commands.MissingPermissions):
     await ctx.send("Sorry, Only my owner, Venimental#1289 can use this command")
+
+keep_alive.keep_alive()
 
 
 token=os.environ.get("botsecret")
